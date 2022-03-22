1afda92def2e3f8515d31a81a1a2cf055ff0de8b
Rutvik sanathara <89308203+RUTVIK-SANATHARA@users.noreply.github.com>
2022-03-22 08:50:07 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,47 @@
+document.getElementById("defaultOpen").click();
+
+function tabOpener(evt, kat) {
+  var i, tabcontent, tablinks;
+
+  // Get all elements with class="tabcontent" and hide them
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+
+  // Get all elements with class="tablinks" and remove the class "active"
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+
+  // Show the current tab, and add an "active" class to the button that opened the tab
+  document.getElementById(kat).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+
+
+// second scroll 
+
+
+document.getElementById("defaultOpen2").click();
+
+function tabOpener2(evt, kat) {
+  var i, tabcontent, tablinks;
+
+  // Get all elements with class="tabcontent" and hide them
+  tabcontent = document.getElementsByClassName("tabcontent2");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+
+  // Get all elements with class="tablinks" and remove the class "active"
+  tablinks = document.getElementsByClassName("tablinks2");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+
+  // Show the current tab, and add an "active" class to the button that opened the tab
+  document.getElementById(kat).style.display = "block";
+  evt.currentTarget.className += " active";
+}
